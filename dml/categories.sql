96560ff3a9fecb45f57977daa9bccff828f37cd7
Raghu Kasturi <raghu@kreeda.co>
2019-09-23 04:11:15 -0600

    KR-5, SQL to handle endpoint

--- dml/categories.sql
+++ dml/categories.sql
@@ -1,1 +1,3 @@
 INSERT INTO category(category, sub_category, category_level, created_by, last_modified_by) VALUES ('Cricket', 'Ball Sports', 'BEGINNER', 1,1);
+
+INSERT INTO category(category, sub_category, category_level, created_by, last_modified_by) VALUES ('Table Tennis', 'Ball Sports', 'BEGINNER', 1,1);
